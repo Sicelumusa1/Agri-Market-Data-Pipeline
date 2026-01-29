14c8881139b094246ca400b5a3bb3cc071e9591b
Sicelumusa <musaqwabe@gmail.com>
2026-01-29 14:40:20 +0200

    create a separate provider for the scraper

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -9,30 +9,32 @@ resource "google_storage_bucket_iam_member" "bucket_writer" {
   member = "serviceAccount:${google_service_account.github_scraper.email}"
 }
 
-resource "google_iam_workload_identity_pool" "github" {
-  workload_identity_pool_id = "github-pool"
-  display_name              = "GitHub Actions Pool"
-}
-
-resource "google_iam_workload_identity_pool_provider" "github" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-provider"
-
-  display_name = "GitHub Provider"
-
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-
+# Create a separate provider for the scraper repo
+resource "google_iam_workload_identity_pool_provider" "github_scraper" {
+  project = var.project_id
+  
+  # Reference the existing pool
+  workload_identity_pool_id          = "github-pool"
+  workload_identity_pool_provider_id = "github-scraper-provider"
+  display_name                       = "GitHub Scraper Provider"
+  
+  attribute_condition = "assertion.repository == \"Sicelumusa1/market-data-ingestion-scraper\""
+  
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
     "attribute.repository" = "assertion.repository"
   }
+  
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
 }
 
-resource "google_service_account_iam_member" "wif_binding" {
+# Then bind to provider
+resource "google_service_account_iam_member" "wif_binding_scraper" {
   service_account_id = google_service_account.github_scraper.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/Sicelumusa1/market-data-ingestion-scraper"
+  
+  # Use the correct numeric project ID
+  member = "principalSet://iam.googleapis.com/projects/384931138677/locations/global/workloadIdentityPools/github-pool/attribute.repository/Sicelumusa1/market-data-ingestion-scraper"
 }
-
