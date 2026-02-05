958f87ae4c7aafb8cf1592a339636641889414a2
Sicelumusa <musaqwabe@gmail.com>
2026-02-05 10:50:56 +0200

    Add base cleaner and it unit tests

---
+++ cleansing/dlt_project/__init__.py
@@ -0,0 +1,8 @@
+"""Market Data Pipeline - Process commodity data from GCS to BigQuery"""
+
+__version__ = "1.0.0"
+__author__ = "Sicelumusa"
+
+# from dlt_project.pipeline import MarketDataPipeline
+
+# __all__ = ["MarketDataPipeline"]
